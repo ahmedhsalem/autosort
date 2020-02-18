bd26880e630a4b0af38c1684d5e7dab9ce6182ac
gimafidon <gimafidon@motivez.co.uk>
2020-02-18 15:34:18 +0000

    Created module to contain our vars/funcs/classes (i.e.serial monitor)

---
+++ module_with_functions.py
@@ -0,0 +1,13 @@
+# Python module with definitions of variables, functions, and classes
+import serial
+
+# Setting up the pyserial module
+
+ser = serial.Serial(
+                    port='COM4',
+                    baudrate=9600,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    timeout=5
+)
